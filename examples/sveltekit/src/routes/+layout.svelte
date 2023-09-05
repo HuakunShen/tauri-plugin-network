0ef05fcc0ffb9c9e0024c932aae6f18a025fcb3d
Huakun Shen <huakun.shen@huakunshen.com>
2023-09-05 01:31:57 -0400

    Add port scanning example

--- examples/sveltekit/src/routes/+layout.svelte
+++ examples/sveltekit/src/routes/+layout.svelte
@@ -3,5 +3,9 @@
 </script>
 
 <div class="p-5 h-screen">
+  <div class="grid grid-cols-4 gap-4">
+    <a class="link" href="/">Network Interface</a>
+    <a class="link" href="/scan">Scan</a>
+  </div>
   <slot />
 </div>
--- examples/sveltekit/tailwind.config.js
+++ examples/sveltekit/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-}
-
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+};
