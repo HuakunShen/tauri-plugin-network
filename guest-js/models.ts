f636dde3ea3e0924ab2a590dd751dc709f5e66fb
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-13 17:58:40 -0400

    use named import for valibot to reduce bundle size

--- guest-js/models.ts
+++ guest-js/models.ts
@@ -1,7 +1,7 @@
-import * as v from "valibot";
+import { number, object, string, type InferOutput } from "valibot"
 
-export const Ipv4Network = v.object({
-  addr: v.string(),
-  prefix: v.number(),
-});
-export type Ipv4Network = v.InferOutput<typeof Ipv4Network>;
+export const Ipv4Network = object({
+  addr: string(),
+  prefix: number()
+})
+export type Ipv4Network = InferOutput<typeof Ipv4Network>
--- guest-js/types.ts
+++ guest-js/types.ts
@@ -1,40 +1,51 @@
-import * as v from "valibot";
+import {
+  array,
+  literal,
+  nullable,
+  number,
+  object,
+  optional,
+  record,
+  string,
+  union,
+  type InferOutput
+} from "valibot"
 
-export const V6IfAddr = v.object({
-  ip: v.string(),
-  ip_octets: v.array(v.number()),
-  broadcast: v.nullable(v.string()),
-  broadcast_octets: v.nullable(v.array(v.number())),
-  netmask: v.nullable(v.string()),
-  netmask_octets: v.nullable(v.array(v.number())),
-  prefix: v.nullable(v.number()),
-  network: v.nullable(v.string()),
-});
-export const V4IfAddr = V6IfAddr;
-export type V4IfAddr = v.InferOutput<typeof V4IfAddr>;
-export type V6IfAddr = v.InferOutput<typeof V6IfAddr>;
-export const Addr = v.record(v.string(), v.union([V4IfAddr, V6IfAddr]));
-export type Addr = v.InferOutput<typeof Addr>;
-export const NetworkInterface = v.object({
-  name: v.string(),
-  v4_addrs: v.array(V4IfAddr),
-  v6_addrs: v.array(V6IfAddr),
-  mac_addr: v.nullable(v.string()),
-  index: v.number(),
-});
-export type NetworkInterface = v.InferOutput<typeof NetworkInterface>;
+export const V6IfAddr = object({
+  ip: string(),
+  ip_octets: array(number()),
+  broadcast: nullable(string()),
+  broadcast_octets: nullable(array(number())),
+  netmask: nullable(string()),
+  netmask_octets: nullable(array(number())),
+  prefix: nullable(number()),
+  network: nullable(string())
+})
+export const V4IfAddr = V6IfAddr
+export type V4IfAddr = InferOutput<typeof V4IfAddr>
+export type V6IfAddr = InferOutput<typeof V6IfAddr>
+export const Addr = record(string(), union([V4IfAddr, V6IfAddr]))
+export type Addr = InferOutput<typeof Addr>
+export const NetworkInterface = object({
+  name: string(),
+  v4_addrs: array(V4IfAddr),
+  v6_addrs: array(V6IfAddr),
+  mac_addr: nullable(string()),
+  index: number()
+})
+export type NetworkInterface = InferOutput<typeof NetworkInterface>
 
-export const IpPortPair = v.object({
-  ip: v.string(),
-  port: v.number(),
-});
-export type IpPortPair = v.InferOutput<typeof IpPortPair>;
+export const IpPortPair = object({
+  ip: string(),
+  port: number()
+})
+export type IpPortPair = InferOutput<typeof IpPortPair>
 
-export const HttpScanOptions = v.object({
-  port: v.number(),
-  keyword: v.optional(v.string()),
-  route: v.optional(v.string()),
-  protocol: v.optional(v.union([v.literal("http"), v.literal("https")])),
-  statusCode: v.optional(v.number()),
-});
-export type HttpScanOptions = v.InferOutput<typeof HttpScanOptions>;
+export const HttpScanOptions = object({
+  port: number(),
+  keyword: optional(string()),
+  route: optional(string()),
+  protocol: optional(union([literal("http"), literal("https")])),
+  statusCode: optional(number())
+})
+export type HttpScanOptions = InferOutput<typeof HttpScanOptions>
