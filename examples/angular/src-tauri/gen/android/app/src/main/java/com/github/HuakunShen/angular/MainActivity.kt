15c24f255aba6531d99369ea31e3cc873c2bfd8d
Sander Lemans <s.lemans@hotraco.com>
2025-11-06 10:57:58 +0100

    add angular example app

---
+++ examples/angular/src-tauri/gen/android/app/src/main/java/com/github/HuakunShen/angular/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.HuakunShen.angular
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
